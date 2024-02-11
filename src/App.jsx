94f63fce50fa17f064a32fb9ce5c412c9f2b6f83
ramez <ramezhishamahmad@gmail.com>
2024-02-12 01:19:04 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { CORE_CONCEPTS } from "./data.JS";
+import Header from "./components/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
