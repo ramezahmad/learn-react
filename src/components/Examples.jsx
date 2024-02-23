5c1819805e5c305b60cdf31cf21665cc068bc55f
ramez <ramezhishamahmad@gmail.com>
2024-02-23 12:29:26 +0300

    Setting Default Prop Values

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -15,7 +15,6 @@ export default function Examples() {
   return (
     <Section title="Examples" id="examples">
       <Tabs
-      ButtonsContainer="menu"
         buttons={
           <>
             <TabButton
